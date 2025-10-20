9a501f425b062a85b7a820744a01ad348ec6be59
clair.w.lawrence@student.uts.edu.au <clair.w.lawrence@student.uts.edu.au>
2025-10-20 04:39:25 +0000

    Created _.jpeg; Edited AssignmentTypeTemplates and Form1

--- client_code/AssignmentTypeTemplates/__init__.py
+++ client_code/AssignmentTypeTemplates/__init__.py
@@ -1,24 +1,14 @@
 from ._anvil_designer import AssignmentTypeTemplatesTemplate
 from anvil import *
-import anvil.tables as tables
-import anvil.tables.query as q
 from anvil.tables import app_tables
-
+from datetime import datetime
 
 class AssignmentTypeTemplates(AssignmentTypeTemplatesTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
+    # Initialize the form
     self.init_components(**properties)
 
-    
-    def button_back_click(self, **event_args):
-      """Go back to the main form/home screen"""
-    # Open Form1
+  def button_back_click(self, **event_args):
+    """Navigate back to main form"""
     open_form('Form1')
 
-    # Import the main form (Form1)
-    from ..Form1 import Form1
-
-    # Navigate back to Form1
-    # This replaces the current screen with Form1
-    open_form('Form1')
