dc122c1a0df8b20f1675411b930f19e062e22c0e
clair.w.lawrence@student.uts.edu.au <clair.w.lawrence@student.uts.edu.au>
2025-10-21 04:18:45 +0000

    Created AssignmentTypeTemplates.ExamGuideTemplate; Edited AssignmentTypeTemplates.EssayTemplate; Deleted AssignmentTypeTemplates.ExamTemplate

--- client_code/AssignmentTypeTemplates/ExamGuideTemplate/__init__.py
+++ client_code/AssignmentTypeTemplates/ExamGuideTemplate/__init__.py
@@ -1,11 +1,11 @@
-from ._anvil_designer import ExamTemplateTemplate
+from ._anvil_designer import ExamGuideTemplateTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class ExamTemplate(ExamTemplateTemplate):
+class ExamGuideTemplate(ExamGuideTemplateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
